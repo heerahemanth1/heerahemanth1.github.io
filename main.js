e6c4513fb42c15ad73bc38ccf7cc25b0707c21a8
Heera Hemanth Bylla <heerahemanth1@gmail.com>
2018-06-26 20:57:12 +0530

    A script to handle the content of the website.

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    var array = {
+        cards: [
+            {
+                title: "Linux From Scratch",
+                subtitle: "For Geeks",
+                image: "",
+                text: "A Linux distribution that allows a great deal of customization as well as control over the Operating System.",
+                links: [
+                    {
+                        text: "Read More...",
+                        url: "http://www.linuxfromscratch.org/lfs/view/stable/",
+                    },
+                ],
+            },
+        ],
+    };
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+});
