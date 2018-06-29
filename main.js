25d18f3fa4fdaca5e24c6055df6c59382b6cef30
Heera Hemanth Bylla <heerahemanth1@gmail.com>
2018-06-29 09:26:38 +0530

    Mr Robot content added.

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
 $(document).ready(function(){
 
     var array = {
-        cards: [
+        "cards": [
             {
-                title: "Linux From Scratch",
-                subtitle: "For Geeks",
-                image: "",
-                text: "A Linux distribution that allows a great deal of customization as well as control over the Operating System.",
-                links: [
+                "title": "Linux From Scratch",
+                "subtitle": "For Geeks",
+                "image": "",
+                "text": "A Linux distribution that allows a great deal of customization as well as control over the Operating System.",
+                "links": [
                     {
-                        text: "Read More...",
-                        url: "http://www.linuxfromscratch.org/lfs/view/stable/",
+                        "text": "Read More...",
+                        "url": "http://www.linuxfromscratch.org/lfs/view/stable/",
+                    },
+                ],
+            },
+            {
+                "title": "Mr Robot",
+                "subtitle": "Story unlike most",
+                "image": "",
+                "text": "A tv series by 'USA Network', 'Universal Cable Productions' and 'Anonymous Content'.",
+                "links": [
+                    {
+                        "text": "A Peek...",
+                        "url": "https://www.google.co.in/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwiOhPWSwvbbAhXEOI8KHQSuAN4QFggoMAA&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FMr._Robot&usg=AOvVaw3QTebE-xL0lrQd7OjL0hz3"
                     },
                 ],
             },
         ],
     };
 
-    for (let index = 0; index < array.length; index++) {
+    for (let index = 0; index < array.cards.length; index++) {
         const element = array[index];
         
     }
